f09551eb9cc2deba40d2d56082b8b286f7d0b446
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-02 14:32:19 +0700

    display horizontal meny

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,28 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dense flat></v-app-bar>
+    <v-app-bar app color="primary" dense flat>
+      <v-container class="py-0 fill-height">
+        <v-btn v-for="link in links" :key="link.target" :to="link.target" dark text>
+          {{ link.name }}
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-responsive class="mr-6" max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+        <v-btn dark text to="/cart">
+          <v-icon>
+            mdi-cart
+          </v-icon>
+        </v-btn>
+      </v-container>
+
+    </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt></Nuxt>
@@ -11,8 +33,13 @@
 
 <script>
 export default {
-  data() {
-    return {}
-  }
+  data: () => ({
+    links: [
+      {name: 'Trang Chủ', target: '/'},
+      {name: 'Của Hàng', target: '/shop'},
+      {name: 'Liên Hệ', target: '/contact'},
+      {name: 'Tài Khoản', target: '/account'},
+    ]
+  })
 }
 </script>
