15b8565c256ebe78cecd7ed36c08a369f12a3c8b
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-03 21:15:42 +0700

    color theme to primary green

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,6 +60,9 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
+        },
+        light: {
+          primary: colors.green.darken2,
         }
       }
     }
