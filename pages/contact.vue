0c847277665a875af7f32ed8925faa7a27e06583
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-04 10:56:08 +0700

    contact

---
+++ pages/contact.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>
+      Liên lạc
+    </h1>
+    <p>
+      Hãy liên lạc cho chúng tôi ngay qua số điện thoại: (+84) 123 456 789
+    </p>
+  </v-container>
+</template>
