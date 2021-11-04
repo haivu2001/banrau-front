f4e5e48ac7f81b773935f6b8bccd83078e95a82c
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-04 18:36:22 +0700

    account menu

---
+++ components/AccountMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-menu
+    bottom
+    offset-y
+    open-on-hover
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        dark
+        text
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>mdi-account</v-icon>
+        Tài Khoản
+      </v-btn>
+    </template>
+
+    <v-list>
+      <template v-if="!isLoggedIn">
+        <v-list-item>
+          <v-list-item-title>
+            <v-btn text>Đăng Nhập</v-btn>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>
+            <v-btn text>Đăng Ký</v-btn>
+          </v-list-item-title>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  props: ['isLoggedIn']
+}
+</script>
