0e5dac5e4cd9722129df958ba25ef6f1f2cee0b0
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-04 18:36:41 +0700

    shop page

---
+++ pages/shop.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="product in purchasableProducts" :key="product.id" class="col-md-4 mt-6">
+        <SingleProduct :product="product"></SingleProduct>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(['purchasableProducts'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
