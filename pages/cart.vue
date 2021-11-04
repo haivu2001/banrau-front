27fea49c4804e66c4ba3ba8560652d09566ef34e
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-04 19:03:40 +0700

    cart page

---
+++ pages/cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ itemsInCart }}
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(['itemsInCart'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
