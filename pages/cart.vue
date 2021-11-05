ef04e94d192f4e04923a548562eefbafd7c2bc9b
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-05 22:09:04 +0700

    display items in cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    {{ itemsInCart }}
-  </div>
+  <v-container>
+    <v-simple-table>
+      <thead>
+      <tr>
+        <th class="text-left">Sản phẩm</th>
+        <th class="text-left">Giá đơn vị</th>
+        <th class="text-left">Số lượng</th>
+        <th class="text-left">Hành động</th>
+      </tr>
+      </thead>
+      <tbody>
+      <CartEntry v-for="id in itemsInCart" :key="id"
+                 :product="purchasableProducts.find((x) => x.id === id)"></CartEntry>
+      </tbody>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>
@@ -9,7 +22,7 @@ import {mapGetters} from "vuex";
 
 export default {
   computed: {
-    ...mapGetters(['itemsInCart'])
+    ...mapGetters(['itemsInCart', 'purchasableProducts'])
   }
 }
 </script>
