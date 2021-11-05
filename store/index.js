043c2f9b4fa50a7bc1584a6a3ed42525e056ace5
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-05 22:09:32 +0700

    states for item amounts

--- store/index.js
+++ store/index.js
@@ -1,22 +1,32 @@
 export const state = () => ({
-  itemsInCart: []
+  itemsInCart: [],
+  itemsAmounts: [],
 })
 
 export const mutations = {
   addItemToCart(state, item) {
     state.itemsInCart.push(item.id)
+  },
+  removeItemFromCart(state, item) {
+    state.itemsInCart = state.itemsInCart.filter((x) => x !== item)
+  },
+  changeAmount(state, payload) {
+    state.itemsAmounts = state.itemsAmounts.filter((x) => x.item !== payload.item)
+    state.itemsAmounts.push(payload)
   }
 }
 
 
 export const getters = {
+  itemAmounts(state) {
+    return state.itemsAmounts
+  },
   isLoggedIn(_state) {
     return false;
   },
   itemsInCart(state) {
     return state.itemsInCart
-  }
-  ,
+  },
   purchasableProducts(_state) {
     return [
       {
