41c4cbcfa058aeac461be3fcfeec5a2ad34c2981
quyetnguyen134 <quyet.nguyen134@hcmut.edu.vn>
2021-11-27 10:27:45 +0700

    checkout update #4

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -297,7 +297,7 @@
 <script>
 (function() {
   "use strict";
-   global.window.addEventListener(
+   globalThis.window.addEventListener(
     "load",
     function() {
       // Fetch all the forms we want to apply custom Bootstrap validation styles to
